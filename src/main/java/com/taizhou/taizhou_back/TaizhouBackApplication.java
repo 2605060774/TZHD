6d81189b765d49a88118f742fc821d55fcf892fa
changjiang <1161268455@qq.com>
2020-11-25 10:33:43 +0800

    once

---
+++ src/main/java/com/taizhou/taizhou_back/TaizhouBackApplication.java
@@ -0,0 +1,13 @@
+package com.taizhou.taizhou_back;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaizhouBackApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TaizhouBackApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/taizhou/taizhou_back/TaizhouBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.taizhou.taizhou_back;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaizhouBackApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
